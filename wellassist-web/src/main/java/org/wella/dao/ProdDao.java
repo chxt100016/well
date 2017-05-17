fde1e4d90d7e1e9ac7330fa438f485758a9ae8f9
gpslw <gpslw@qq.com>
2017-05-17 16:49:13 +0800

    prodDao修改

--- wellassist-web/src/main/java/org/wella/dao/ProdDao.java
+++ wellassist-web/src/main/java/org/wella/dao/ProdDao.java
@@ -3,10 +3,8 @@ package org.wella.dao;
 import org.apache.ibatis.annotations.Param;
 import org.wella.entity.Prod;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
->>>>>>> 6a71c2f098ca5b59d32ecf37c6434c00fb48b163
+
 import java.util.Map;
 
 /**
@@ -20,11 +18,8 @@ public interface ProdDao {
      */
     Prod getOrderProdByProdid(@Param("prodId")long prodId);
 
-<<<<<<< HEAD
     List<Prod> findProd(Map map);
 
     List<Map<String,Object>> findUserProdList(Map map);
-=======
     Map<String,Object> findProdById(Map param);
->>>>>>> 6a71c2f098ca5b59d32ecf37c6434c00fb48b163
 }
