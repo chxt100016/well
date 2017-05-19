b831547bbe60be294b71f08815dd60daabb32854
gpslw <gpslw@qq.com>
2017-05-19 16:42:51 +0800

    测试

--- wellassist-web/src/main/java/org/wella/platform/controller/MemberController.java
+++ wellassist-web/src/main/java/org/wella/platform/controller/MemberController.java
@@ -8,6 +8,7 @@ import io.wellassist.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.wella.dao.WaUserDao;
+import org.wella.entity.Prod;
 import org.wella.entity.User;
 import org.wella.platform.service.impl.MemberServiceImpl;
 
@@ -65,12 +66,12 @@ public class MemberController extends AbstractController{
     }
 
     /**
-     * 获取卖家详情
+     * 用户信息查看
      * @param
      * @return
      */
-    @RequestMapping("sellerinfo/{userId}")
-    public R sellerInfo(@PathVariable("userId") Long userId){
+    @RequestMapping("userinfo/{userId}")
+    public R userInfo(@PathVariable("userId") Long userId){
         Map<String,Object> params = new HashMap();
         params.put("userId",userId);
         Map user = menberServiceImpl.findUserInfoById(userId);
@@ -79,23 +80,21 @@ public class MemberController extends AbstractController{
 
     /**
      * 获取卖家产品列表
-     * @param params
+     *
      * @return
      */
-    @RequestMapping("productList")
-    public R productList(@RequestParam Map<String,Object> params){
+    @RequestMapping("productList/{userId}")
+    public R productList(@RequestParam Map<String,Object> params,@PathVariable("userId") Long userId){
+        List<Prod> prodList = this.menberServiceImpl.findProductsByUserId(userId);
 
-        params.put("userType", "3");
-        List<User> sellerList = this.waUserDao.findUser(params);
-        int totalCount = sellerList.size();
+//        List<User> sellerList = this.waUserDao.findUser(params);
+//        int totalCount = sellerList.size();
 
         //查询列表数据
         Query query = new Query(params);
-//        List<SysUserEntity> userList = sysUserService.queryList(query);
-//        int total = sysUserService.queryTotal(query);
-        PageUtils pageUtil = new PageUtils(sellerList, totalCount, query.getLimit(), query.getPage());
-
+        PageUtils pageUtil = new PageUtils(prodList, prodList.size(), query.getPage(), query.getLimit());
         return R.ok().put("page", pageUtil);
+
     }
 
     /**
