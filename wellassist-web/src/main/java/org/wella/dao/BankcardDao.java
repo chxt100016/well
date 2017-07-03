9bbac01e5a05d3905530b52c8e8306accab7cee2
xuqinghuo <xuqinghuo@126.com>
2017-07-03 11:29:53 +0800

    提交21

--- wellassist-web/src/main/java/org/wella/dao/BankcardDao.java
+++ wellassist-web/src/main/java/org/wella/dao/BankcardDao.java
@@ -14,6 +14,7 @@ import java.util.Map;
 public interface BankcardDao {
     List<Bankcard> getCardListByUserId(long userId);
 
+
     long addCard(Map<String,Object> map);
 
     int delCard(long bankcardId);
