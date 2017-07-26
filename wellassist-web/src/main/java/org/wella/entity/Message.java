c2a79f3795979dff23343fee64089ef50e772aed
xuqinghuo <xuqinghuo@126.com>
2017-07-26 10:52:32 +0800

    添加消息

---
+++ wellassist-web/src/main/java/org/wella/entity/Message.java
@@ -0,0 +1,45 @@
+package org.wella.entity;
+
+/**
+ * Created by xuqinghuo on 2017/7/25.
+ */
+public class Message {
+
+    private Integer id;
+    private String title;
+    private String type;
+    private String content;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
