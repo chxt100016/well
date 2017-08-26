e609bca713162cafdfd469ad9b7902a47067b4fe
Tom <ranmo1212@qq.com>
2017-08-26 11:45:27 +0800

    征信计算 展示页 录入页

--- wellassist-web/src/main/webapp/js/common.js
+++ wellassist-web/src/main/webapp/js/common.js
@@ -92,3 +92,18 @@ function getSelectedRows() {
 }
 
 var baseUrl="/wellassist/";
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
