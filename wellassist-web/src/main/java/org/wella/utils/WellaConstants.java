91185ff96afeca5d79401eb03b2f344609be98c9
dingjianwen <zzzowdp@163.com>
2017-09-08 18:13:25 +0800

    submit

--- wellassist-web/src/main/java/org/wella/utils/WellaConstants.java
+++
@@ -1,8 +0,0 @@
-package org.wella.utils;
-
-/**
- * Created by Administrator on 2017/9/6.
- */
-public class WellaConstants {
-    public static final String CNCB_SERVER_BASEURL="http://192.168.0.181:8079/";
-}
